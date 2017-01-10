88ab752fd88ce6caba19b3da8b8055550deba829
Ravi Teja <raviteja.bhupatiraju@nih.gov>
2017-01-10 10:45:41 -0500

    json is in 3

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
-import string, cherrypy
+import cherrypy
 from pymongo import MongoClient
-from bson.son import SON
-from collections import OrderedDict
 from json import dumps
 
 db = MongoClient()['pubmed']
@@ -9,42 +7,39 @@ db = MongoClient()['pubmed']
 def counts(term_str):
     a = 1965
     b = 2015
-    print(term_str)
+
     terms = [s.strip() for s in term_str.split(',')]
-    res = db.article.aggregate(
-       [
-        {
-          '$match': {
-            'year': {'$gt': a - 1, '$lt': b + 1 },
-            'mesh': {'$in': terms}
-            }
-        },
-        {
-          '$group': {
-             '_id': "$year",
-             "count": {"$sum": 1}
-          }
-        },
-        {'$sort': {'_id': 1}}
-       ]
-    )
-    d = {}; wres = {}
-    for r in res:
-        d[r['_id']] = r['count']
-    for i in range(a, b + 1):
-        if i in d:
-            wres[i] = d[i]
-        else:
-            wres[i] = 0
-    out = dumps([{'x':k, 'y':wres[k]} for k in wres])
+    terms.extend([term_str])
+
+    results = []
+    for term in terms:
+        qterms = [s.strip() for s in term_str.split(',')] if ',' in term else qterms = [term]
+
+        res = db.article.aggregate(
+           [
+            {
+              '$match': {
+                'year': {'$gt': a - 1, '$lt': b + 1 },
+                'mesh': {'$in': qterms}
+                }
+            },
+            {
+              '$group': {
+                 '_id': "$year",
+                 "count": {"$sum": 1}
+              }
+            },
+            {'$sort': {'_id': 1}}
+           ]
+        )
+        values = [{'x': r['_id'], 'y': r['count']} for r in res]
+        results.append({'key': 'co-occurrence', 'values': values}) if ',' in term else results.append({'key': term, 'values': values})
+
     return dumps({
         "status": "SUCCESS",
-        "data": [{
-            "key": terms,
-            "values": out
-            },
-        ]
+        "data": results
     })
+
 class HelloWorld(object):
 
     @cherrypy.expose()
@@ -57,3 +52,4 @@ class HelloWorld(object):
 
 
 cherrypy.quickstart(HelloWorld())
+#print(counts('Electroretinography,Neoplasm Metastasis'))
