575acba68fb63e0e040dcdc582305787beba2318
Satya <satyajeet.raje@gmail.com>
2017-01-11 10:42:44 -0500

    mesh freq query

--- wordcloud.py
+++ wordcloud.py
@@ -1,9 +1,7 @@
-db.article.aggregate(
-               [
-                {$match: {'year': {'$gt': 1965, '$lt': 2010 },'mesh': {'$in': ['Ebolavirus']}}},
-                {$project: { _id: 0, mesh: 1 } },
-  				{$unwind: "$mesh" },
-                {$group: {'_id': "$mesh",count: {"$sum": 1}}},
-                {$sort: {'_id': 1}}
-               ]
-            )
+res = db.article.aggregate([
+	{'$match': {'year': {'$gt': 2010, '$lt': 2015}}},
+    {'$project': {'_id': 0, 'mesh': 1}},
+    {'$unwind': '$mesh'},
+    {'$group': {'_id': "$mesh", 'count': {'$sum': 1}}},
+    {'$sort': {'count': -1}},
+])
