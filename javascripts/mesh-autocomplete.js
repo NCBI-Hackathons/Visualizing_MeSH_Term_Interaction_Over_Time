268eedc675e90c8fe3f0398f01d32f1c2fc1b7f3
Abdelrahman <abdelrahman.hosny@hotmail.com>
2017-01-11 10:27:42 -0500

    autocomplete debug

--- javascripts/mesh-autocomplete.js
+++ javascripts/mesh-autocomplete.js
@@ -1,6 +1,43 @@
 $( function() {
-    $( "#mesh-terms" ).autocomplete({
-        source: "/auto_complete",
-        appendTo: $("#mesh-terms")
-    });
+    /*$( "#mesh-terms" ).autocomplete({
+        source: "/auto_complete"
+    });*/
+    function split( val ) {
+      return val.split( /,\s*/ );
+    }
+    function extractLast( term ) {
+      return split( term ).pop();
+    }
+ 
+    $( "#mesh-terms" )
+      // don't navigate away from the field on tab when selecting an item
+      .on( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).autocomplete( "instance" ).menu.active ) {
+          event.preventDefault();
+        }
+      })
+      .autocomplete({
+        minLength: 0,
+        source: function( request, response ) {
+          // delegate back to autocomplete, but extract the last term
+          response( $.ui.autocomplete.filter(
+            availableTags, extractLast( request.term ) ) );
+        },
+        focus: function() {
+          // prevent value inserted on focus
+          return false;
+        },
+        select: function( event, ui ) {
+          var terms = split( this.value );
+          // remove the current input
+          terms.pop();
+          // add the selected item
+          terms.push( ui.item.value );
+          // add placeholder to get the comma-and-space at the end
+          terms.push( "" );
+          this.value = terms.join( ", " );
+          return false;
+        }
+      });
 });
